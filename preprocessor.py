e338f9373fd5c8286041ebbfbc462677d2095002
Hugo Mesones <hugomesones21@gmail.com>
2022-11-29 14:58:34 +0100

    Hugo preprocessing arbres BA

--- preprocessor.py
+++ preprocessor.py
@@ -1,20 +1,42 @@
 def data_cleaning_ba(data_ba):
-  '''
-  Permet de nettoyer le dataset de bueno aires
-  Difficile de généraliser la fonction à cause des colonnes qui sont différentes dans les différents df (paris par exemple)
-  '''
-  #Drop columns
-  columns_to_drop = [ 'tipo_activ','manzana','calle_nombre', 'calle_altura', 'calle_chapa', 'direccion_normalizada',
-                     'ubicacion',  'ancho_acera', 'estado_plantera','ubicacion_plantera', 'nivel_plantera']
-  data_ba = data_ba.drop(columns=columns_to_drop)
+    '''
+    Allow you to clean Bueno Aires trees dataset.
+    Take the dataframe as an input to return a dataframe with the
+    This function is specific
 
-  #rename columns
-  data_ba.columns = ['long', 'lat', 'id', 'arrondissement', 'nom_scientifique', 'diametre_cm', 'hauteur_m']
+    Difficile de généraliser la fonction à cause des colonnes qui sont différentes dans les différents df (paris par exemple)
+    '''
+    #Drop columns
+    columns_to_drop = [ 'tipo_activ','manzana','calle_nombre', 'calle_altura', 'calle_chapa', 'direccion_normalizada',
+                        'ubicacion',  'ancho_acera', 'estado_plantera','ubicacion_plantera', 'nivel_plantera']
+    data_ba = data_ba.drop(columns=columns_to_drop)
 
-  #Compute radius
-  data_ba['rayon'] = data_ba['diametre_cm'].apply(lambda x: x/2)
+    #rename columns
+    data_ba.columns = ['long', 'lat', 'id', 'arrondissement', 'nom_scientifique', 'diametre_cm', 'hauteur_m']
 
-  #Remove 'No identificado'
-  data_ba = data_ba[data_ba['nom_scientifique'] != 'No identificado']
+    #Compute radius
+    data_ba['rayon'] = data_ba['diametre_cm'].apply(lambda x: x/2)
 
-  return data_ba
+    #Remove 'No identificado'
+    data_ba = data_ba[data_ba['nom_scientifique'] != 'No identificado']
+
+    # Remove trees that are
+    data_ba = data_ba[(data_ba['hauteur_m'] > 0) & (data_ba['diametre_cm'] > 0)]
+
+    return data_ba
+
+def clean(df):
+    '''
+    Allow you to clean the dataframe with the right columns
+    '''
+    #Suppression des NaNs
+    df = df[['diametre_cm', 'hauteur_cm']].dropna()
+
+    #Nettoyage des valeurs
+    df['nom_scientifique'] = df['nom_scientifique'].str.lower()
+    df['nom_scientifique'] = df['nom_scientifique'].str.strip()
+
+    #Sélection des arbres
+    #df[(df['hauteur_m'] >= 1) & (df['diametre_cm'] >= 1)]
+
+    return df
