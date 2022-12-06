862febd5c455a75dc99e635c1a52d758291db03c
helene d orleans <heleneorleans@gmail.com>
2022-12-06 16:04:12 +0100

    update

--- streamlit/pages/page_04.py
+++ streamlit/pages/page_04.py
@@ -14,19 +14,6 @@ data_paris , data_ba = get_data()
 paris_clean = cleaning_paris(data_paris)
 ba_clean = cleaning_ba(data_ba)
 
-@st.cache
-def get_map_paris():
-    return paris_clean[['lat', 'lon']]
-
-@st.cache
-def get_map_ba():
-    return ba_clean[['lat', 'lon']]
-
-map_paris = get_map_paris()
-map_ba = get_map_ba()
-
-st.map(map_paris)
-st.map(map_ba)
 
 st.markdown("""
     ## Focus on the Carbon stock vs the GHG emissions
