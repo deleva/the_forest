b17d021310f632cea89805a65fe849d6d51d590d
EVANGELINA DE LUCA <evangelinadeluca@gmail.com>
2022-12-09 15:39:47 +0100

    make pack

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
     content = f.readlines()
 requirements = [x.strip() for x in content if "git+" not in x]
 
-setup(name='the_forest',
+setup(name='forest',
       version="0.1",
       description="Install_package",
       license="MIT",
